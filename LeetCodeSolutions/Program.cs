bfd77be86fe9db2f8d8d60c7482ccb56cdfa081b
Valentin Popov <valorrin@gmail.com>
2023-08-29 13:20:22 +0300

    TwoSum

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", Solutions.TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
+        }
+    }
+}
---
+++ LeetCodeSolutions/Solutions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class Solutions
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int y = i + 1; y < nums.Length; y++)
+                {
+                    if (nums[i] + nums[y] == target)
+                    {
+                        result[0] = i;
+                        result[1] = y;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
