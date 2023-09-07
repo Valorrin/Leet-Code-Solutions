72b65114ca795fd04b401306e632a572c55d170e
Valentin Popov <valorrin@gmail.com>
2023-09-07 22:31:03 +0300

    Valid Parentheses

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -7,8 +7,9 @@ namespace LeetCodeSolutions
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine(String.Join(",", Solutions.TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
-            Console.WriteLine(Solutions.LengthOfLongestSubstring("pwwkew").ToString());
+            // Console.WriteLine(String.Join(",", Solutions.TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
+            // Console.WriteLine(Solutions.LengthOfLongestSubstring("pwwkew").ToString());
+            Console.WriteLine(Solutions.ValidParentheses("(]"));
         }
     }
 }
