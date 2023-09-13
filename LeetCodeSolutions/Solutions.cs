7f6d3bd85dc1277e6bbb27eaf2a8846bf3e94302
Valentin Popov <valorrin@gmail.com>
2023-09-13 19:32:05 +0300

    Contains Duplicate

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -61,22 +61,15 @@ namespace LeetCodeSolutions
 
         public static bool ValidParentheses(string s)
         {
-            // Get ready initial state (enforce element type)
-            var k = new Stack<char>();
-            // Evaluate each character for potential mismatch 
+            var stack = new Stack<char>();
             foreach (char c in s)
             {
-                // Push closing round bracket onto the stack
-                if (c == '(') { k.Push(')'); continue; }
-                // Push closing curly bracket onto the stack
-                if (c == '{') { k.Push('}'); continue; }
-                // Push closing square bracket onto the stack
-                if (c == '[') { k.Push(']'); continue; }
-                // Look out for imbalanced strings or mismatches
-                if (k.Count == 0 || c != k.Pop()) return false;
+                if (c == '(') { stack.Push(')'); continue; }
+                if (c == '{') { stack.Push('}'); continue; }
+                if (c == '[') { stack.Push(']'); continue; }
+                if (stack.Count == 0 || c != stack.Pop()) return false;
             }
-            // Empty stack means string is valid and invalid otherwise
-            return k.Count == 0;
+            return stack.Count == 0;
         }
     }
 }
