688d08740cf91b5852e3f1756a4afa89d4316059
Valentin Popov <valorrin@gmail.com>
2023-10-11 19:10:51 +0300

    Best Time to Buy and Sell Stock

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -347,6 +348,28 @@ namespace LeetCodeSolutions
                 return false;
             }
 
+        }
+
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+
+            return maxProfit;
         }
 
     }
