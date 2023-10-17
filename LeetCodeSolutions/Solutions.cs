8814ca0b114edb1086882eb8ddbd68c67e45e9e3
Valentin Popov <valorrin@gmail.com>
2023-10-17 22:33:00 +0300

    Pascal's Triangle

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -350,7 +350,7 @@ namespace LeetCodeSolutions
 
         }
 
-        public int MaxProfit(int[] prices)
+        public static int MaxProfit(int[] prices)
         {
             int minPrice = int.MaxValue;
             int maxProfit = 0;
@@ -370,6 +370,25 @@ namespace LeetCodeSolutions
             }
 
             return maxProfit;
+        }
+
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            int[][] result = new int[numRows][];
+            int i = 0;
+            while (i < numRows)
+            {
+                result[i] = new int[i + 1];
+                result[i][0] = result[i][i] = 1;
+                int j = 1;
+                while (j < i)
+                {
+                    result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+                    j++;
+                }
+                i++;
+            }
+            return result;
         }
 
     }
