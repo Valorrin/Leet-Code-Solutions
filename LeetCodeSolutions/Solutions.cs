59e5f4d24424188d86e5853d2de80854f2ba5d91
Valentin Popov <valorrin@gmail.com>
2023-11-03 23:05:54 +0200

    Remove Duplicates from Sorted Array

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -445,5 +445,22 @@ namespace LeetCodeSolutions
             return newNumber;
         }
 
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int k = 1;
+            int previouse = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != previouse)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+                previouse = nums[i];
+            }
+            return k;
+
+        }
+
     }
 }
