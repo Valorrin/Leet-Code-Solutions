22dde42b7824326058f0308339ed3bd1f9caa550
Valentin Popov <valorrin@gmail.com>
2023-11-20 23:19:40 +0200

    Single Number

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -462,5 +462,14 @@ namespace LeetCodeSolutions
 
         }
 
+        public int SingleNumber(int[] nums)
+        {
+            int res = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+                res ^= nums[i];
+
+            return res;
+        }
     }
 }
