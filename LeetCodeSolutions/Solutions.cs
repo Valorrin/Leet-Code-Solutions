9426621853fe86b856664679ecb0fd9e21313cfa
Valentin Popov <valorrin@gmail.com>
2023-11-23 23:53:53 +0200

    Ugly Number

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -462,7 +462,7 @@ namespace LeetCodeSolutions
 
         }
 
-        public int SingleNumber(int[] nums)
+        public static int SingleNumber(int[] nums)
         {
             int res = nums[0];
 
@@ -497,6 +497,16 @@ namespace LeetCodeSolutions
                 sum += currentDigit * currentDigit;
             }
             return sum;
+        }
+
+        public bool IsUgly(int n)
+        {
+            if (n <= 0) return false;
+
+            while (n % 2 == 0) n = n / 2;
+            while (n % 3 == 0) n = n / 3;
+            while (n % 5 == 0) n = n / 5;
+            return n == 1;
         }
     }
 }
