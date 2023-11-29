dcf22cf49ccde45b3a1c92020eacd5e46c50f384
Valentin Popov <valorrin@gmail.com>
2023-11-29 21:24:23 +0200

    Summary Ranges

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -508,5 +508,25 @@ namespace LeetCodeSolutions
             while (n % 5 == 0) n = n / 5;
             return n == 1;
         }
+
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            var result = new List<string>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int start = nums[i];
+                while (i < nums.Length - 1 && nums[i + 1] - nums[i] == 1)
+                {
+                    i++;
+                }
+                if (start != nums[i])
+                    result.Add($"{start}->{nums[i]}");
+                else
+                    result.Add($"{start}");
+            }
+
+            return result;
+
+        }
     }
 }
