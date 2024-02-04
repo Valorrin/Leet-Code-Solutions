2f7dd52d645b29df48933466a25e4685b9017ad8
Valentin Popov <valorrin@gmail.com>
2024-02-04 10:35:29 +0200

    Remove Element

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -886,5 +886,20 @@ namespace LeetCodeSolutions
 
             return sb.ToString();
         }
+
+        public int RemoveElement(int[] nums, int val)
+        {
+            int result = 0;
+            foreach (var item in nums)
+            {
+                if (item != val)
+                {
+                    nums[result] = item;
+                    result++;
+                }
+            }
+
+            return result;
+        }
     }
 }
