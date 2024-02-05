6835e95121bafa037a707ef512e9f8af6e0fe714
Valentin Popov <valorrin@gmail.com>
2024-02-05 23:37:23 +0200

    Find the Index of the First Occurrence in a String

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -901,5 +901,18 @@ namespace LeetCodeSolutions
 
             return result;
         }
+
+        public int StrStr(string haystack, string needle)
+        {
+            for (var i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                if (haystack.Substring(i, needle.Length) == needle)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
     }
 }
