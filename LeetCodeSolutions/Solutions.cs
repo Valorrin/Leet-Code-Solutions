28c36072f1b35cca6465f8b6ba7bdb0d8631e47f
Valentin Popov <valorrin@gmail.com>
2024-02-07 23:12:57 +0200

    Search Insert Position

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -930,5 +930,19 @@ namespace LeetCodeSolutions
             }
             return dp[n];
         }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1, mid;
+            while (left <= right)
+            {
+                mid = (right + left) / 2;
+                if (nums[mid] < target) left = mid + 1;
+                else if (nums[mid] > target) right = mid - 1;
+                else return mid;
+            }
+            return left;
+        }
+
     }
 }
