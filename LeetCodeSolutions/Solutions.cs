b16afac9350a707fa02a99279a96cf1b818284c8
Valentin Popov <valorrin@gmail.com>
2024-02-08 23:17:30 +0200

    Simplify Path

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -944,5 +944,30 @@ namespace LeetCodeSolutions
             return left;
         }
 
+        public string SimplifyPath(string path)
+        {
+            String[] p = path.Split('/');
+            List<string> sList = new();
+            foreach (var px in p)
+            {
+                if (string.IsNullOrEmpty(px) || px == ".")
+                    continue;
+                if (px == "..")
+                {
+                    if (sList.Count > 0)
+                        sList.RemoveAt(sList.Count - 1);
+
+                }
+                else
+                    sList.Add(px);
+            }
+            StringBuilder sb = new();
+            foreach (var s in sList)
+                sb.Append("/" + s);
+            if (sb.Length == 0)
+                sb.Append("/");
+            return sb.ToString();
+        }
+
     }
 }
