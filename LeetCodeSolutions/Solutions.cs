134e9ba208a407ba1cd54cf1b1ee204991344718
Valentin Popov <valorrin@gmail.com>
2024-02-13 23:47:56 +0200

    Search in Rotated Sorted Array II

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -984,5 +984,47 @@ namespace LeetCodeSolutions
             return replaceIndex;
         }
 
+        public bool Search81(int[] nums, int k)
+        {
+            int low = 0, high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (nums[mid] == k) return true;
+
+                if (nums[low] == nums[mid] && nums[mid] == nums[high])
+                {
+                    low++; high--;
+                    continue;
+                }
+                if (nums[low] <= nums[mid])
+                {
+                    if (nums[low] <= k && k <= nums[mid])
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] <= k && k <= nums[high])
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+
+            }
+
+            return false;
+        }
+
     }
 }
